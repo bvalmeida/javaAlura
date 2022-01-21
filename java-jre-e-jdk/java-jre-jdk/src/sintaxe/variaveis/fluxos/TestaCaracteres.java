910938e24b5bfcd64a65b3155d8289e7a7470f17
Breno Vieira <bvalmeida94@gmail.com>
2022-01-21 13:45:00 -0300

    estudando tipos primitivos

---
+++ java-jre-e-jdk/java-jre-jdk/src/sintaxe/variaveis/fluxos/TestaCaracteres.java
@@ -0,0 +1,12 @@
+package sintaxe.variaveis.fluxos;
+
+public class TestaCaracteres {
+	public static void main (String [] args) {
+		
+		char letra = 'a';
+		char valor = 66;
+		
+		System.out.println(letra);
+		System.out.println(valor);
+	}
+}
---
+++ java-jre-e-jdk/java-jre-jdk/src/sintaxe/variaveis/fluxos/TestaConversao.java
@@ -0,0 +1,19 @@
+package sintaxe.variaveis.fluxos;
+
+public class TestaConversao {
+	public static void main(String [] args) {
+		
+		double salario = 1270.50;
+		float prontoFlutuante = 3.14f;
+		int valor = (int)salario;
+		
+		System.out.println(valor);
+		
+		double valor1 = 0.2;
+		double valor2 = 0.1;
+		double total = valor1 + valor2;
+		
+		System.out.println(total);
+	}
+
+}
